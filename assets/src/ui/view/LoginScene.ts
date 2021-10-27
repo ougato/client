610f0c8bf62d48cca7ad7ec3ddf1b546bcd1bac8
ougato <ougato@gmail.com>
2021-10-28 02:27:47 +0800

    增加登陆场景

--- assets/src/ui/view/LoginScene.ts
+++ assets/src/ui/view/LoginScene.ts
@@ -2,14 +2,12 @@
  * Author       : ougato
  * Date         : 2021-09-04 23:39:20
  * LastEditors  : ougato
- * LastEditTime : 2021-09-08 00:31:17
+ * LastEditTime : 2021-10-27 23:36:19
  * FilePath     : /client/assets/src/ui/view/LoginScene.ts
  * Description  : 
  */
 
-import MarioScene from "../../../games/mario/src/MarioScene";
 import BaseScene from "../../core/base/BaseScene";
-import * as BundleDefine from "../../core/define/BundleDefine";
 import HallScene from "./HallScene";
 
 const { ccclass, property } = cc._decorator;
@@ -20,19 +18,19 @@ export default class LoginScene extends BaseScene {
     // 预制路径
     public static prefabPath: string = "scene/LoginScene";
 
-    // onLoad () {
-    // }
+    onLoad () {
+
+    }
 
     start() {
         
     }
 
-    private onClickMario(): void {
-        G.UIMgr.openWaiting();
-        // G.UIMgr.openScene({
-        //     sceneClass: HallScene,
-        //     bundleName: BundleDefine.Name.RESOURCES,
-        // })
+    /**
+     * 点击游客登录
+     */
+    private onClickLoginGuest(): void {
+        
     }
 
     // update (dt) {}
